79e38476ea81026b1736f26f0a95205ec877f2c0
Liuwx <liuwx126@sina.com>
2017-02-19 20:27:29 +0800

    实现毛玻璃效果，并且点击图片查看原图

--- FrostedGlass/FrostedGlass/ViewController.m
+++ FrostedGlass/FrostedGlass/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 
+#import "ZoomImageView.h"
 @interface ViewController ()
 
 @end
@@ -16,9 +17,45 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIImageView * imageview = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"张圆圆_02.jpg"]];
+    ZoomImageView *imageVIew1 = [[ZoomImageView alloc] init];
+    
+    imageVIew1.image = [UIImage imageNamed:@"张圆圆_02.jpg"];
+    imageview.frame = CGRectMake(0, 0, self.view.frame.size.width, 300);
+    imageview.userInteractionEnabled = YES;
+    imageview.contentMode = UIViewContentModeScaleAspectFit;
+    
+    if (imageVIew1.image.size.height / imageVIew1.image.size.width > imageview.frame.size.height / imageview.frame.size.width) {
+        
+        CGFloat width = imageVIew1.image.size.width * imageview.frame.size.height / imageVIew1.image.size.height;
+        imageVIew1.frame = CGRectMake((imageview.frame.size.width - width) / 2, 0,width, imageview.frame.size.height);
+        
+    }else {
+        
+        CGFloat height = imageVIew1.image.size.height * imageview.frame.size.width / imageVIew1.image.size.width;
+        imageVIew1.frame = CGRectMake(0, (imageview.frame.size.height - height) / 2, imageview.frame.size.width, height);
+        
+    }
+    [self.view addSubview:imageview];
+    [self.view addSubview:imageVIew1];
+    UIBlurEffect *blur = [UIBlurEffect effectWithStyle:UIBlurEffectStyleExtraLight];
+    
+    UIVisualEffectView *effectview = [[UIVisualEffectView alloc] initWithEffect:blur];
+    
+    effectview.frame = imageview.frame;
+    
+    [imageview addSubview:effectview];
+    
+    
+    
+    
+    
+    
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
